7e9ac760f9838a854b0a9be33971a8be4e288778
NotDom979 <blkwhte979@gmail.com>
2022-07-11 19:07:41 -0400

    ButtonFactoryMerged
    Small revision to window.h. Removal of unused code

--- Calculator/Window.h
+++ Calculator/Window.h
@@ -4,27 +4,7 @@
 class Window : public wxFrame
 {
 private:
-	wxButton* Add = nullptr;
-	wxButton* Subtract = nullptr;
-	wxButton* Multiply = nullptr;
-	wxButton* Divide = nullptr;
-	wxButton* Mod = nullptr;
-	wxButton* Clear = nullptr;
-	wxButton* Negative = nullptr;
-	wxButton* Decimal = nullptr;
-	wxButton* Hex = nullptr;
-	wxButton* Bin = nullptr;
-	wxButton* Equal = nullptr;
-	wxButton* Num1 = nullptr;
-	wxButton* Num2 = nullptr;
-	wxButton* Num3 = nullptr;
-	wxButton* Num4 = nullptr;
-	wxButton* Num5 = nullptr;
-	wxButton* Num6 = nullptr;
-	wxButton* Num7 = nullptr;
-	wxButton* Num8 = nullptr;
-	wxButton* Num9 = nullptr;
-	wxButton* Num0 = nullptr;
+	
 	wxTextCtrl* Numberbox = nullptr;
 	wxDECLARE_EVENT_TABLE();
 	void OnButton(wxCommandEvent& event);
